3719b66588885e64ad1caa0e560ff755aa4674a3
HadyAlzben <moqtnane@gmail.com>
2023-08-15 23:43:28 +0300

    Add new Relations

--- Entity/Food.cs
+++ Entity/Food.cs
@@ -3,7 +3,8 @@ namespace FinalProject.Entity
     public class Food : BaseEntity
     {
         public string Name { get; set; }
-        public string ShopId { get; set; }
+        public string OwnerId { get; set; }
+        public User Owner { get; set; }
         public float Price { get; set; }
         public string Description { get; set; }
     }
--- Entity/Order.cs
+++ Entity/Order.cs
@@ -6,11 +6,22 @@ namespace FinalProject.Entity
 
         public int Phone { get; set; }
 
+
         public float TotalPrice { get; set; }
+
+        public string BasketId { get; set; }
+
+        public Basket Basket { get; set; }
 
         public string CurrentStatus { get; set; }
 
+        public string OwnerId { get; set; }
+
+        public User OwnerData { get; set; }
+
         public DateTime CreatedAt { get; set; }
+
+
 
 
     }
