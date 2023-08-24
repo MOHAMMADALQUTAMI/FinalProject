236491f28a0f264ae82d4df0071594dafea5a5ff
HadyAlzben <moqtnane@gmail.com>
2023-08-24 22:24:59 +0300

    add New Ui

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,19 +8,57 @@ import { AppComponent } from './app.component';
 
 import { AuthGuard } from './guards/auth.guard';
 import { MainPageComponent } from './web/main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './web/footer/footer.component';
+import { HeaderComponent } from './web/header/header.component';
+import { NotFoundComponent } from './web/not-found/not-found.component';
+import { ProductDetailComponent } from './web/product-detail/product-detail.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { LoginComponent } from './web/login/login.component';
+import { RegisterComponent } from './web/register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    FooterComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    ProductDetailComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatCardModule,
+    FlexLayoutModule,
     HttpClientModule,
+    MatIconModule,
     FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
     RouterModule.forRoot([
       { path: '', canActivate: [AuthGuard], component: MainPageComponent, },
-      { path: '**', component: AppComponent, },
+      { path: 'product', component: ProductDetailComponent, },
+      { path: 'login', canActivate: [AuthGuard], component: LoginComponent, },
+      { path: 'register', canActivate: [AuthGuard], component: RegisterComponent, },
+      { path: '**', component: NotFoundComponent, },
     ])
   ],
   providers: [],
--- ClientApp/src/app/web/main-page/main-page.component.ts
+++ ClientApp/src/app/web/main-page/main-page.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+  products = [
+    {
+      title: 'Product 1',
+      image: 'house_default.png',
+      price: '$19.99'
+    },
+    {
+      title: 'Product 2',
+      image: 'house_default.png',
+      price: '$29.99'
+    },
+    {
+      title: 'Product 3',
+      image: 'house_default.png',
+      price: '$9.99'
+    },
+    {
+      title: 'Product 3',
+      image: 'house_default.png',
+      price: '$9.99'
+    },
+    {
+      title: 'Product 3',
+      image: 'house_default.png',
+      price: '$9.99'
+    },
+    {
+      title: 'Product 3',
+      image: 'house_default.png',
+      price: '$9.99'
+    },
+    {
+      title: 'Product 3',
+      image: 'house_default.png',
+      price: '$9.99'
+    },
+    {
+      title: 'Product 3',
+      image: 'house_default.png',
+      price: '$9.99'
+    },
 
+  ];
 }
+
